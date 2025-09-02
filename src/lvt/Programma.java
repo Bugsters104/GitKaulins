2310ecbc7405abd683ee4b02ca8bef99ce6a941c
Bugsters104 <vards.uzvards@pasts.lv>
2025-09-02 12:02:01 +0300

    Izveidot metode kauliņa mešanai

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -3,11 +3,13 @@ package lvt;
 import java.util.Random;
 
 public class Programma {
+	static void mestKaulinu() {
+		Random rand = new Random();
+		int skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
 
 	public static void main(String[] args) {
-		int skaitlis;
-		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		mestKaulinu();
 	}
 }
